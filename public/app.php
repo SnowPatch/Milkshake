a889411ba9e05a81cda9adc936b470227bed0682
SnowPatch <sven.w.bachmann@gmail.com>
2021-09-23 14:08:29 +0200

    Remove debug code

--- public/app.php
+++ public/app.php
@@ -5,10 +5,6 @@ use Milkshake\DotEnv;
 use Milkshake\Controller;
 use Milkshake\Router;
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 session_start();
 require_once '../system/Loader.php';
 
